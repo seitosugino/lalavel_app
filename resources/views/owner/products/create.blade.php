cfd93ed12e00752e8add3715f1262da9c19e8556
seitosugino <0jb4f61k3853v2f@gmail.com>
2022-01-29 00:03:08 +0900

    product store

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -21,7 +21,7 @@
                       </div>
                       <div class="p-2 w-1/2 mx-auto">
                         <div class="relative">
-                          <label for="information" class="leading-7 text-sm text-gray-600">店舗情報 ※必須</label>
+                          <label for="information" class="leading-7 text-sm text-gray-600">商品情報 ※必須</label>
                           <textarea id="information" name="information" rows="10" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ old('information') }}</textarea>
                         </div>
                       </div>
